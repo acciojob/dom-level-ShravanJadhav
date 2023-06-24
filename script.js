ff44a46da5cbb5e42fb7478b857accd5554ae940
Shravan Jadhav <96675283+ShravanJadhav@users.noreply.github.com>
2023-06-24 20:41:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+window.onload = function() {
+  var targetElement = document.getElementById('level');
+  var domLevel = getDOMLevel(targetElement);
+  alert('The level of the element is: ' + domLevel);
+};
+
+function getDOMLevel(element) {
+  var level = 0;
+  var currentElement = element;
+
+  while (currentElement.parentElement) {
+    level++;
+    currentElement = currentElement.parentElement;
+  }
+
+  return level;
+}
